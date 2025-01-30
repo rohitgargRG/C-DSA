ff59508a39af66743b8323d824d34fb2fc1ee38f
rohitgargRG <gargrohit2003@gmail.com>
2025-01-30 21:40:12 +0530

    completed maxHeap insertion

---
+++ Trees/heap/maxHeap.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class MaxHeap{
+
+    int* arr;
+    int size;  // total elements in heap
+    int total_size; // total size of array
+
+    public:
+
+    MaxHeap(int n)
+    {
+        arr = new int[n];
+        size = 0;
+        total_size = n;
+    }
+
+    // insert into heap
+
+    void insert(int value)
+    {
+        // if heap size is avialable or not
+        if(size == total_size)
+        {
+            cout<<"heap overflow\n";
+            return;
+        }
+
+        arr[size] = value;
+        int index = size; // index where we have inserted the element
+        size++;
+
+        // compare it with its parent
+        while(index > 0 && arr[(index-1) / 2] < arr[index]){  // parent < child
+            swap(arr[(index-1) / 2] , arr[index]);
+            index = (index - 1)/2;
+        }
+
+        cout<<arr[index]<<" inserted in heap\n";
+    }
+
+    void print()
+    {
+        for(int i = 0; i < size ; i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+    }
+};
+
+int main() {
+
+    MaxHeap H1(6);
+    H1.insert(4);
+    H1.insert(14);
+    H1.insert(11);
+    H1.print();
+    H1.insert(114);
+    H1.insert(24);
+    H1.insert(1);
+    H1.print();
+    return 0;
+}
