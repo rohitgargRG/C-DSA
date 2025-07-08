3f7d67dfde85558eea03ed0a66f633740fe566de
rohitgargRG <gargrohit2003@gmail.com>
2025-07-08 08:08:23 +0530

    implementation using adjacency list started

---
+++ Revision/Graphs/imp_list.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// implementation of Graph using adjacency list    
+int main() {
+
+    int vertex,edges;
+    cin>>vertex>>edges;
+
+    vector<vector<int>> adj(vertex);
+    // vector<int> adj[vertex];
+
+    // case 1 - undirected unweighted graph
+    return 0;
+}
