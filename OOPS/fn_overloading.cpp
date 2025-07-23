379fd391ce6ac4d189a3d220a1fd675b9ec6b572
rohitgargRG <gargrohit2003@gmail.com>
2025-07-23 21:11:50 +0530

    operator overloading done

--- OOPS/fn_overloading.cpp
+++ OOPS/fn_overloading.cpp
@@ -11,11 +11,15 @@ class Area{
     int calc_area(int l,int b){ // area of rectangle
         return l*b;
     }
+
+    // error
+    // double calc_area(int r){
+    //     return 3.14*r*r;
+    // }
 };
 int main() {
     Area obj1;
     cout<<obj1.calc_area(5)<<endl;
     cout<<obj1.calc_area(4,5)<<endl;
-
     return 0;
 }
