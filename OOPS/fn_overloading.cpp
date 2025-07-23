209db387b6d0e9d7f53c6100c93f7f07be949adb
rohitgargRG <gargrohit2003@gmail.com>
2025-07-23 20:27:13 +0530

    function overloading done

---
+++ OOPS/fn_overloading.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Area{
+    public:
+
+    int calc_area(int r){ // area of circle
+        return 3.14*r*r;
+    }
+
+    int calc_area(int l,int b){ // area of rectangle
+        return l*b;
+    }
+};
+int main() {
+    Area obj1;
+    cout<<obj1.calc_area(5)<<endl;
+    cout<<obj1.calc_area(4,5)<<endl;
+
+    return 0;
+}
