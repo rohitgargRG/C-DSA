f58b3a03662ae1d4660bba54b58ee92f1673b3bc
rohitgargRG <gargrohit2003@gmail.com>
2025-07-29 06:14:32 +0530

    reverse array using 1 and 2 variable using reursion done

--- Recursion/reverse_array.cpp
+++ Recursion/reverse_array.cpp
@@ -1,19 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-void reverse_array(int i , int j , vector<int>& arr){
+// using 2 variables
+// void reverse_array(int i , int j , vector<int>& arr){
+//     // base
+//     if(i >= j) return;
+
+//     // swap
+//     int temp = arr[i];
+//     arr[i] = arr[j];
+//     arr[j] = temp;
+
+//     reverse_array(i + 1 , j - 1 , arr);
+// }
+
+// using 1 variable
+void reverse_array(int i , vector<int>& arr){
     // base
-    if(i >= j) return;
+    if(i >= arr.size()/2) return;
 
     // swap
     int temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
+    arr[i] = arr[arr.size()-1-i];
+    arr[arr.size()-1-i] = temp;
 
-    reverse_array(i + 1 , j - 1 , arr);
+    reverse_array(i + 1 , arr);
 }
 int main() {
     vector<int> arr = {1,2,3,4,5};
-    reverse_array(0,arr.size()-1,arr);
+    reverse_array(0,arr);
     for(auto it : arr) cout<<it<<" ";
     return 0;
 }
